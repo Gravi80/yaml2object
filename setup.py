f723a723f88c62f8e38e8af0742e9f51699191ae
Ravi Sharma <ravis4376@gmail.com>
2018-11-30 22:57:48 +0530

    bumping up version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class install(_install):
 if __name__ == '__main__':
     setup(
         name='yaml2object',
-        version='1.0.9',
+        version='1.0.10',
         description='A simple solution that allows dot notation for YAML file.',
         long_description=read_file('README.md'),
         long_description_content_type="text/markdown",
